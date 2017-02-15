d916278393dd6765b73e3200d3591ee977471d5f
Raphaël Rougeron <goldoraf@gmail.com>
2017-02-15 11:59:12 +0100

    [feat] Added photo selection

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -5,15 +5,27 @@ import { Link, withRouter } from 'react-router'
 
 import { STACK_FILES_DOWNLOAD_PATH } from '../constants/config'
 
-export const Photo = ({ photo, router }) => {
+export const Photo = ({ photo, selected = false, onToggle, router }) => {
   const parentPath = router.location.pathname
   return (
-    <Link to={`${parentPath}/${photo._id}`}>
-      <img
-        className={styles['pho-photo-item']}
-        src={`${STACK_FILES_DOWNLOAD_PATH}/${photo._id}`}
-      />
-    </Link>
+    <div>
+      <span data-input='checkbox'>
+        <input
+          type='checkbox'
+          checked={selected}
+         />
+        <label onClick={e => {
+          e.stopImmediatePropagation()
+          onToggle(photo._id, selected)
+        }} />
+      </span>
+      <Link to={`${parentPath}/${photo._id}`}>
+        <img
+          className={styles['pho-photo-item']}
+          src={`${STACK_FILES_DOWNLOAD_PATH}/${photo._id}`}
+        />
+      </Link>
+    </div>
   )
 }
 
