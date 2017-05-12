119d9c63eaf1cdec445e70079e5add1c801f570e
GoOz <gooz@foojin.com>
2017-05-12 14:16:21 +0200

    feat: Add autofocus on input + few minor tweaks

--- src/components/NewAlbum.jsx
+++ src/components/NewAlbum.jsx
@@ -8,29 +8,31 @@ import { translate } from '../lib/I18n'
 export const NewAlbum = ({ t }) => {
   return (
     <div className={styles['pho-panel']}>
-      <header className={styles['pho-panel-header']}>
-        <div className={styles['pho-panel-wrap']}>
-          <label className={styles['coz-form-label']}>{t('Albums.create.panel_form.label')}</label>
-          <input type='text' name='' id='' value={t('Albums.create.panel_form.placeholder')} />
-        </div>
-      </header>
-      <div className={styles['pho-panel-content']}>
-        <div className={styles['pho-panel-wrap']}>
-          PhotoBoard
-        </div>
-      </div>
-      <footer className={styles['pho-panel-footer']}>
-        <div className={styles['pho-panel-wrap']}>
-          <div className={styles['pho-panel-controls']}>
-            <button className={classNames('coz-btn', 'coz-btn--secondary')}>
-              {t('Albums.create.panel_form.cancel')}
-            </button>
-            <button className={classNames('coz-btn', 'coz-btn--regular')}>
-              {t('Albums.create.panel_form.submit')}
-            </button>
+      <form action='' className={styles['pho-panel-form']}>
+        <header className={styles['pho-panel-header']}>
+          <div className={styles['pho-panel-wrap']}>
+            <label className={styles['coz-form-label']}>{t('Albums.create.panel_form.label')}</label>
+            <input type='text' name='' id='' autofocus value={t('Albums.create.panel_form.placeholder')} />
+          </div>
+        </header>
+        <div className={styles['pho-panel-content']}>
+          <div className={styles['pho-panel-wrap']}>
+            PhotoBoard
           </div>
         </div>
-      </footer>
+        <footer className={styles['pho-panel-footer']}>
+          <div className={styles['pho-panel-wrap']}>
+            <div className={styles['pho-panel-controls']}>
+              <button className={classNames('coz-btn', 'coz-btn--secondary')}>
+                {t('Albums.create.panel_form.cancel')}
+              </button>
+              <button className={classNames('coz-btn', 'coz-btn--regular')}>
+                {t('Albums.create.panel_form.submit')}
+              </button>
+            </div>
+          </div>
+        </footer>
+      </form>
     </div>
   )
 }
